4b41c924f59ff075fc545beb21cadb1a03eb2758
jacomartins <jaco.alu.lmb@gmail.com>
2024-06-09 21:23:35 -0300

    feat: Organização dos projetos para preparar-me para entrevista de emprego

--- web/src/components/Pages/Home/index.tsx
+++ web/src/components/Pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, ReactNode } from 'react';
 import {
   PresentSection, PresentContainer, PageMain, AboutMeSection, AboutMeContainer, AbilitiesSection, SectionTitle, SectionText, AbilitiesContainer, ProjectsSection, ProjectsContainer, Section, SectionContainer, Container, SubSection, SectionH1, ColoredText, SectionH2, Button, Footer
-
 } from './styles';
 
 import { Summary } from '../../Summary';
@@ -13,6 +12,7 @@ import { Logo } from '../../Header/styles';
 import { BookBookmark, Bus } from 'phosphor-react';
 
 import curriculo from '../../../assets/documents/CV - Jacó Martins.pdf'
+import portmelogo from '../../../assets/images/portme40x.svg'
 
 interface PageType {
   name: string;
@@ -150,27 +150,53 @@ export function Page({ name, colorScheme }: PageType) {
         </SectionContainer>
       </Section>
 
-      <SubSection id='projectsbookyourself' height='40rem' bg={colorScheme === 'light' ? '#F0FFF4' : '#0c1411'} colorScheme={colorScheme}>
+      <SubSection id='moovooca' height='40rem' bg={colorScheme === 'light' ? '#e6faf3' : '#131414'} colorScheme={colorScheme}>
+        <Container row="row-reverse">
+          <aside>
+            <Bus size='16rem' weight='regular' color="#338274" />
+            <div className='TextContainer'>
+              <SectionH1 color='#338274'>Moovooca</SectionH1>
+              <SectionH2 color={colorScheme === 'light' ? 'var(--text-light)' : 'var(--text-dark)'}>O Moovooca é uma tentativa de projeto inovador destinado a auxiliar os alunos da UFC a encontrarem a linha ideal de ônibus. Com a ambição de oferecer uma solução eficiente e confiável, o Moovooca visa funcionar consistentemente na maior parte dos momentos, proporcionando aos usuários uma experiência de transporte fluida e sem contratempos.</SectionH2>
+              <Button color='#338274'>Saiba mais sobre em github.com</Button>
+            </div>
+          </aside>
+        </Container>
+      </SubSection>
+
+      <SubSection id='port.me' height='40rem' bg={colorScheme === 'light' ? '#D8B9FF' : '#0c1411'} colorScheme={colorScheme}>
+        <Container row="row">
+          <aside>
+            <img src={portmelogo} alt="Logomarca do Port.me" />
+            <div className='TextContainer'>
+              <SectionH1 color="#8F5CFF">port.me</SectionH1>
+              <SectionH2 color={colorScheme === 'light' ? 'var(--text-light)' : 'var(--text-dark)'}>O BookYourself é uma plataforma destinada a melhora, automatização e gerência de reservas de itens ou ambientes de uma instituição.</SectionH2>
+              <Button color={colorScheme === 'light' ? '#8F5CFF' : '#8F5CFF'}>Saiba mais em github.com</Button>
+            </div>
+          </aside>
+        </Container>
+      </SubSection>
+
+      <SubSection id='moovooca' height='40rem' bg={colorScheme === 'light' ? '#F0FFF4' : '#0c1411'} colorScheme={colorScheme}>
+        <Container row="row">
+          <aside>
+            <Bus size='16rem' weight='regular' color="#38A169" />
+            <div className='TextContainer'>
+              <SectionH1 color="#38A169">Moovooca</SectionH1>
+              <SectionH2 color={colorScheme === 'light' ? 'var(--text-light)' : 'var(--text-dark)'}></SectionH2>
+              <Button color={colorScheme === 'light' ? '#38A169' : '#2F855A'}>Saiba mais em github.com</Button>
+            </div>
+          </aside>
+        </Container>
+      </SubSection>
+
+      <SubSection id='bookyourself' height='40rem' bg={colorScheme === 'light' ? '#F0FFF4' : '#0c1411'} colorScheme={colorScheme}>
         <Container row="row">
           <aside>
             <BookBookmark size='16rem' weight='regular' color="#38A169" />
             <div className='TextContainer'>
               <SectionH1 color="#38A169">BookYourself</SectionH1>
               <SectionH2 color={colorScheme === 'light' ? 'var(--text-light)' : 'var(--text-dark)'}>O BookYourself é uma plataforma destinada a melhora, automatização e gerência de reservas de itens ou ambientes de uma instituição.</SectionH2>
-              <Button color={colorScheme === 'light' ? '#38A169' : '#2F855A'}>Saiba mais</Button>
-            </div>
-          </aside>
-        </Container>
-      </SubSection>
-
-      <SubSection id='projectsecos' height='40rem' bg={colorScheme === 'light' ? '#e6faf3' : '#131414'} colorScheme={colorScheme}>
-        <Container row="row-reverse">
-          <aside>
-            <Bus size='16rem' weight='regular' color="#338274" />
-            <div className='TextContainer'>
-              <SectionH1 color='#338274'>Moovooca</SectionH1>
-              <SectionH2 color={colorScheme === 'light' ? 'var(--text-light)' : 'var(--text-dark)'}>Uma plataforma com o intuito de guiar os alunos da UFC quanto as linhas de ônibus, seus horários e como chegar até a faculdade.</SectionH2>
-              <Button color='#338274' onClick={() => window.open('https://jacomartins.vercel.app/main/core/ui/ecos.html')}>Saiba mais</Button>
+              <Button color={colorScheme === 'light' ? '#38A169' : '#2F855A'}>Saiba mais em github.com</Button>
             </div>
           </aside>
         </Container>
@@ -185,15 +211,15 @@ export function Page({ name, colorScheme }: PageType) {
 
                 {cert.map((item) => {
                   return (
-                      <IconButton
-                        key={item.description}
-                        text={item.text}
-                        description={item.description}
-                        image={item.image}
-                        link={item.link}
-                        type={item.type}
-                        colorScheme={colorScheme}
-                      />
+                    <IconButton
+                      key={item.description}
+                      text={item.text}
+                      description={item.description}
+                      image={item.image}
+                      link={item.link}
+                      type={item.type}
+                      colorScheme={colorScheme}
+                    />
                   )
                 })}
 
